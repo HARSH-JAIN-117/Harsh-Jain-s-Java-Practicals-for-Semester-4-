c03b6ca2fdeac95f726a2410a5877ef8844db295
Harsh Jain <150881689+HARSH-JAIN-117@users.noreply.github.com>
2024-03-30 15:52:41 +0530

    Create PRACTICAL_01.java
    java prac 1

---
+++ PRACTICAL_01.java
@@ -0,0 +1,7 @@
+// Write a Java Program to Print "Hello World! This is my First Java Program."
+
+public class PRACTICAL_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
